ea8dfb92f657d9720a316f1320669a44531cf6a8
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2019-01-30 13:13:02 +0100

    Declare FileTypeValidator interface

---
+++ src/lordmara/magicnumbers/validator/FileTypeValidator.java
@@ -0,0 +1,15 @@
+package lordmara.magicnumbers.validator;
+
+import lordmara.magicnumbers.exception.UnsupportedFileType;
+import lordmara.magicnumbers.filereader.FileExtensionAndNameReader;
+import lordmara.magicnumbers.filereader.FileSignatureReader;
+import lordmara.magicnumbers.model.FileType;
+
+import java.io.IOException;
+
+public interface FileTypeValidator {
+    FileType getFileType();
+    FileSignatureReader getFileSignatureReader();
+    FileExtensionAndNameReader getFileExtensionAndNameReader();
+    boolean validateFile() throws IOException, UnsupportedFileType;
+}
