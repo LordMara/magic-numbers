922f592bd240b057e6a452fe03dafab1ab718416
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2019-01-30 12:16:01 +0100

    Declare supported file types with extensions, offset and hex signatures

---
+++ src/lordmara/magicnumbers/model/FileType.java
@@ -0,0 +1,17 @@
+package lordmara.magicnumbers.model;
+
+public enum FileType {
+    GIF(new String[]{"GIF"}, 0, new String[]{"474946383761", "474946383961"}),
+    JPG(new String[]{"JPG", "JPEG"}, 0, new String[]{"FFD8FF"}),
+    TXT(new String[]{"TXT"}, 0, new String[]{"EFBBBF", "FFFE", "FEFF", "FFFE0000", "0000FEFF"});
+
+    private final String[] extensions;
+    private final int offset;
+    private final String[] hexSignatures;
+
+    FileType(String[] extensions, int offset, String[] hexSignatures) {
+        this.extensions = extensions;
+        this.offset = offset;
+        this.hexSignatures = hexSignatures;
+    }
+}
