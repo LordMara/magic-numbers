d3cd2b7916f3e0d678af3d0ec470bd25a3fcfc5f
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2019-01-30 14:09:36 +0100

    Implement showValidationResult() method, declare getPossibleExtensions()

--- src/lordmara/magicnumbers/view/ViewImpl.java
+++ src/lordmara/magicnumbers/view/ViewImpl.java
@@ -4,6 +4,15 @@ public class ViewImpl implements View {
     @Override
     public void showValidationResult(boolean validationResult, String fileName,
                                      String fileExtension, String[] foundTypeExtension) {
+        if(validationResult) {
+            System.out.println(String.format("File: %s is true: .%s", fileName, fileExtension));
+        } else {
+            System.out.println(String.format("File: %s pretend to be: .%s but for real is: %s",
+                    fileName, fileExtension, getPossibleExtensions(foundTypeExtension)));
+        }
+    }
 
+    private String getPossibleExtensions(String[] foundTypeExtension) {
+        
     }
 }
